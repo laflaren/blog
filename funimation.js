f56ab2776e03e2980646532f60a9a0e489896352
Natalie La Flare <69825896+laflaren@users.noreply.github.com>
2020-12-24 13:35:06 -0600

    Delete funimation.js

--- funimation.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("header").style.fontSize = "10%";
-  } else {
-    document.getElementById("header").style.fontSize = "20%";
-  }
-}
