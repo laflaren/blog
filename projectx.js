92b73376366873b777a41ee7ffe21311fd124420
Natalie La Flare <69825896+laflaren@users.noreply.github.com>
2021-02-11 13:29:13 -0600

    Update projectx.js
    added list-making and deleting js

--- projectx.js
+++ projectx.js
@@ -1,6 +1,42 @@
 
-  
-  
+  // select all elements 
+const form = document.querySelector('#new-item-form')
+
+const list = document.querySelector('#list')
+
+const input = document.querySelector('#item-input')
+
+
+
+// when I submit form, add a new element 
+form.addEventListener('submit', e => {
+    e.preventDefault()
+
+    console.log(input.value)
+
+
+
+// create new item 
+
+const item = document.createElement('div')
+item.innerText = input.value
+item.classList.add('list-item')
+
+// add item to list 
+list.appendChild(item)
+
+
+// clear input
+input.value = ""
+
+// setup event listened to delete item when clicked.
+item.addEventListener('click', () => {
+    item.remove()
+})
+
+})
+
+
 ar x = blue; 
   drawBox(x);
   drawBox(x);
